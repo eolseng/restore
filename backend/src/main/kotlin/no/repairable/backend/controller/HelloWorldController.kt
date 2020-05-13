11f3480ae17d8ea1b1184151a3478a3f271f8d86
Truls <trulsistre@gmail.com>
2020-05-13 13:43:13 +0200

    create backend package setup

--- backend/src/main/kotlin/no/repairable/backend/controller/HelloWorldController.kt
+++ backend/src/main/kotlin/no/repairable/backend/controller/HelloWorldController.kt
@@ -1,4 +1,4 @@
-package no.repairable.backend
+package no.repairable.backend.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
