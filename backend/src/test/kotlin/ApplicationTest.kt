e89a33ad6ce815610e92c605f5b941c5588e71c2
Truls <trulsistre@gmail.com>
2020-05-15 12:43:54 +0200

    Implement automatic documentation via tests

---
+++ backend/src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,12 @@
+package no.repairable
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestingRestdocsApplicationTests {
+    @Test
+    @Throws(Exception::class)
+    fun contextLoads() {
+    }
+}
---
+++ backend/src/test/kotlin/no/repairable/backend/controller/HelloWorldControllerTest.kt
@@ -0,0 +1,29 @@
+package no.repairable.backend.controller
+
+import org.hamcrest.Matchers
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+
+@WebMvcTest(HelloWorldController::class)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
+class HelloWorldControllerTest {
+    @Autowired
+    private val mockMvc: MockMvc? = null
+
+    @Test
+    @Throws(Exception::class)
+    fun checkReturnedValue() {
+        mockMvc!!.perform(MockMvcRequestBuilders.get("/api/v1/helloworld")).andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Hello world")))
+                .andDo(MockMvcRestDocumentation.document("home"))
+    }
+}
+
