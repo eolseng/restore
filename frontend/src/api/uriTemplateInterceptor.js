d659bd252a498a3737093efc5d8ddcfbcd8317bd
Erik Olseng <e.olseng@gmail.com>
2020-05-21 13:26:55 +0200

    Commented out buggy files uriTemplateInterceptor.js and uriListConverter.js

--- frontend/src/api/uriTemplateInterceptor.js
+++ frontend/src/api/uriTemplateInterceptor.js
@@ -1,18 +1,18 @@
 /* eslint no-undef: "off"*/
-define(function(require) {
-
-	const interceptor = require('rest/interceptor');
-
-	return interceptor({
-		request: function (request /*, config, meta */) {
-			/* If the URI is a URI Template per RFC 6570 (https://tools.ietf.org/html/rfc6570), trim out the template part */
-			if (request.path.indexOf('{') === -1) {
-				return request;
-			} else {
-				request.path = request.path.split('{')[0];
-				return request;
-			}
-		}
-	});
-
-});
+// define(function(require) {
+//
+// 	const interceptor = require('rest/interceptor');
+//
+// 	return interceptor({
+// 		request: function (request /*, config, meta */) {
+// 			/* If the URI is a URI Template per RFC 6570 (https://tools.ietf.org/html/rfc6570), trim out the template part */
+// 			if (request.path.indexOf('{') === -1) {
+// 				return request;
+// 			} else {
+// 				request.path = request.path.split('{')[0];
+// 				return request;
+// 			}
+// 		}
+// 	});
+//
+// });
