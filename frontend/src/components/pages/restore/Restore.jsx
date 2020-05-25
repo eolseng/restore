176c6b4a2b3ac1d67d724dc1ff5cb5593de01840
Erik Olseng <e.olseng@gmail.com>
2020-05-25 22:52:30 +0200

    Add RestoreProgress.js

--- frontend/src/components/pages/restore/Restore.jsx
+++ frontend/src/components/pages/restore/Restore.jsx
@@ -5,31 +5,44 @@ import BrandSelector from "./BrandSelector";
 import ProductCondition from "./ProductCondition";
 import DeliverySelector from "./DeliverySelector";
 import OrderConfirmation from "./OrderConfirmation";
+import RestoreProgress from "./RestoreProgress";
 
 function Restore() {
 
     const {state, dispatch} = useContext(RestoreContext);
 
     const maxSteps = 4;
-    useEffect(() => {
-        dispatch({type: "setMaxSteps", payload: 4})
-    }, [maxSteps]);
+    useEffect(() => {
+        dispatch({type: "setMaxSteps", payload: maxSteps})
+    }, [dispatch, maxSteps]);
 
-
+    let component;
     switch (state.step) {
         case 0:
-            return <BrandSelector/>
+            component = <BrandSelector/>
+            break;
         case 1:
-            return <ProductCondition/>
+            component = <ProductCondition/>
+            break;
         case 2:
-            return <ProductCondition/>
+            component = <ProductCondition/>
+            break;
         case 3:
-            return <DeliverySelector/>
+            component = <DeliverySelector/>
+            break;
         case 4:
-            return <OrderConfirmation/>
+            component = <OrderConfirmation/>
+            break;
         default:
-            new Error("RestoreContext: Step not defined")
+            new Error("RestoreContext: Step not defined. This should not happen...")
     }
+
+    return (
+        <React.Fragment>
+            <RestoreProgress/>
+            {component}
+        </React.Fragment>
+    );
 }
 
 export default Restore;
