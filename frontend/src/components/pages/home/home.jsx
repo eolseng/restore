998118f4328e0722ad5799c015cfd25c8b89945f
Jonas Say <jonassay@Jonass-MacBook-Pro.local>
2020-05-26 14:20:59 +0200

    deleted some .jsx and .css files

--- frontend/src/components/pages/home/home.jsx
+++ frontend/src/components/pages/home/home.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 
-import WelcomeText from "./welcomeText";
-import Flow from "./flow";
 
 /** CSS Imports */
 import "../../../css/pages/home/home.css";
-import "../../../css/pages/home/main.css";
 
 function Home() {
   return (
     <div className='content-container col flex-grow-1'>
-      <WelcomeText />
-      <Flow />
+
     </div>
   );
 }
