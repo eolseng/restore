7c4b7a08f5a338ec178e5ed2ee136e09e6d351bb
Leifhaa <Leifhaa@hotmail.com>
2020-05-26 11:15:04 +0200

    Fixing conflict.

--- frontend/src/productfinder/productlist.js
+++ frontend/src/productfinder/productlist.js
@@ -1,23 +1,20 @@
-import React from 'react'
-import { ProductCard } from './productCard'
-
-import '../css/pages/product-finder/product-list.css'
+import React from "react";
+import {ProductCard} from "./productCard"
 
 export function ProductList(props) {
+
     /*
         Gets passed a LIST of Products as a prop and renders these as individual ProductCards in a container.
      */
 
-    const productList = props.products
-    const productCards = productList.embedded ? (
-        productList.embedded.products.map((product) => <ProductCard key={product.id} product={product} />)
-    ) : (
-        <div className='product-list-no-product'>Could not find any products.</div>
-    )
+    const productList = props.products;
+    const productCards = productList ? productList.map(product =>
+            <ProductCard key={product.id} product={product}/>) :
+        <div className={"product-list-no-product"}>Could not find any products.</div>
 
     return (
-        <div className='product-list-wrapper col-sm-9'>
-            <div className='product-list'>{productCards}</div>
+        <div className={"product-list"}>
+            {productCards}
         </div>
     )
 }
