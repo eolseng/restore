6718500c7ec7c13fcee776c89efb04993ec354e6
Haakon Hess <haakon.hess@gmail.com>
2020-05-26 15:45:34 +0200

    Removed header from index, removed container-styling, initial index structure

--- frontend/src/components/pages/home/home.jsx
+++ frontend/src/components/pages/home/home.jsx
@@ -1,15 +1,52 @@
-import React from "react";
-
+import React from 'react'
 
 /** CSS Imports */
-import "../../../css/pages/home/home.css";
+import '../../../css/pages/home/home.css'
+import { Link } from 'react-router-dom'
 
-function Home() {
-  return (
-    <div className='content-container col flex-grow-1'>
+function showContent() {
 
-    </div>
-  );
 }
 
-export default Home;
+function Home() {
+    return (
+        <div className='home-wrapper'>
+            <div className='above-fold container-fluid'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='above-fold-text-wrapper col-sm-6'>
+                          <h1>Tittel her, kom å redd verden</h1>
+                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia laboriosam et quasi error nobis, temporibus a recusandae placeat veritatis maxime.</p>
+                          <Link to="/filter">
+                            <button className="cta-button">Start nå</button>
+                          </Link>
+                        </div>
+                        <div className='above-fold-img-wrapper col-sm-6'>
+                            <img src={require('../../../img/placeholder.png')} alt={'Hero'} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='container-fluid'>
+                <div className='home-header'>
+                    <button onClick={showContent}>Noe</button>
+                    <button onClick={showContent}>Noe annet</button>
+                    <button onClick={showContent}>Litt mer</button>
+                </div>
+                <div className='container'>
+                    <div className='tab-1'>
+                        <p>Noe</p>
+                    </div>
+                    <div className='tab-2'>
+                        <p>Noe annet</p>
+                    </div>
+                    <div className='tab-3'>
+                        <p>Litt mer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,89 +1,76 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 
-import Header from "./components/layout/header";
-import Footer from "./components/layout/footer";
-import Home from "./components/pages/home/home";
-import { ProductPage } from "./ProductPage";
-import Profile from "./components/pages/profile/profile";
-import Filter from "./components/pages/filter/filter";
-import Condition from "./components/pages/condition/condition";
-import Delivery from "./components/pages/delivery/delivery";
-import Confirmation from "./components/pages/confirmation/confirmation";
+import Header from './components/layout/header'
+import Footer from './components/layout/footer'
+import Home from './components/pages/home/home'
+import { ProductPage } from './ProductPage'
+import Profile from './components/pages/profile/profile'
+import Filter from './components/pages/filter/filter'
+import Condition from './components/pages/condition/condition'
+import Delivery from './components/pages/delivery/delivery'
+import Confirmation from './components/pages/confirmation/confirmation'
 
 /** CSS Imports */
-import "./css/style.css";
+import './css/style.css'
 
 /** Font Awesome Icons */
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faBars,
-  faTimes,
-  faLaptop,
-  faSave,
-  faEdit,
-} from "@fortawesome/free-solid-svg-icons";
-import { faUserCircle } from "@fortawesome/free-regular-svg-icons";
-import { faFacebookSquare, faTwitterSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars, faTimes, faLaptop, faSave, faEdit } from '@fortawesome/free-solid-svg-icons'
+import { faUserCircle } from '@fortawesome/free-regular-svg-icons'
+import { faFacebookSquare, faTwitterSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 /** Font Awesome Icon Library */
-library.add(
-  faBars,
-  faTimes,
-  faLaptop,
-  faSave,
-  faEdit,
-  faUserCircle,
-  faFacebookSquare,
-  faTwitterSquare,
-  faLinkedin
-);
+library.add(faBars, faTimes, faLaptop, faSave, faEdit, faUserCircle, faFacebookSquare, faTwitterSquare, faLinkedin)
 
 function notFound() {
-  return (
-    <div>
-      <h2>NOT FOUND: 404</h2>
-      <p>ERROR: the page you requested in not available.</p>
-    </div>
-  );
+    return (
+        <div>
+            <h2>NOT FOUND: 404</h2>
+            <p>ERROR: the page you requested in not available.</p>
+        </div>
+    )
 }
 
 function App() {
-  return (
-    <React.StrictMode>
-      <BrowserRouter>
-        <div id='main-container'>
-          <Header />
-          <Switch>
-            {/* Husk å legge inn routen i no.repairable.backend.controller.ReactForwardController */}
-            <Route exact path={"/"}>
-              {Home}
-            </Route>
-            <Route exact path={"/profile"}>
-              {Profile}
-            </Route>
-            <Route exact path={"/filter"}>
-              {Filter}
-            </Route>
-            <Route exact path={"/condition"}>
-              {Condition}
-            </Route>
-            <Route exact path={"/delivery"}>
-              {Delivery}
-            </Route>
-            <Route exact path={"/confirmation"}>
-              {Confirmation}
-            </Route>
-            <Route exact path='/product' render={(props) => <ProductPage {...props} />} /> }
-            <Route path='/404' component={notFound} />
-            <Redirect to='/404' />
-          </Switch>
-          <Footer />
-        </div>
-      </BrowserRouter>
-    </React.StrictMode>
-  );
+
+  const path = window.location.pathname;
+
+    return (
+        <React.StrictMode>
+            <BrowserRouter>
+                <div id='main-container'>
+                  {path !== "/" && <Header />}
+                    <Switch>
+                        {/* Husk å legge inn routen i no.repairable.backend.controller.ReactForwardController */}
+                        <Route exact path={'/'}>
+                            {Home}
+                        </Route>
+                        <Route exact path={'/profile'}>
+                            {Profile}
+                        </Route>
+                        <Route exact path={'/filter'}>
+                            {Filter}
+                        </Route>
+                        <Route exact path={'/condition'}>
+                            {Condition}
+                        </Route>
+                        <Route exact path={'/delivery'}>
+                            {Delivery}
+                        </Route>
+                        <Route exact path={'/confirmation'}>
+                            {Confirmation}
+                        </Route>
+                        <Route exact path='/product' render={props => <ProductPage {...props} />} /> }
+                        <Route path='/404' component={notFound} />
+                        <Redirect to='/404' />
+                    </Switch>
+                    <Footer />
+                </div>
+            </BrowserRouter>
+        </React.StrictMode>
+    )
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
