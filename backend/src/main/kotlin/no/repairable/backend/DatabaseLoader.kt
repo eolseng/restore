eb9d478f5f5fb6e0b021975ee095936452a8412e
Truls <trulsistre@gmail.com>
2020-05-27 09:22:22 +0200

    remove most of databaseLoader code and now use ProductsCreationController to populate database on startup

--- backend/src/main/kotlin/no/repairable/backend/DatabaseLoader.kt
+++ backend/src/main/kotlin/no/repairable/backend/DatabaseLoader.kt
@@ -6,20 +6,15 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import no.repairable.backend.controller.ProductsCreationController
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 import java.io.File
+import javax.annotation.PostConstruct
 
 @Component
 class DatabaseLoader @Autowired constructor(
         private val productsCreationController: ProductsCreationController
-) : CommandLineRunner {
-
-
-    override fun run(vararg args: String?) {
-        onStartDataBaseLoader()
-    }
-
+) {
+    @PostConstruct
     fun onStartDataBaseLoader() {
         val mapper = jacksonObjectMapper()
         mapper.registerKotlinModule()
