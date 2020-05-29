2a4fe17f4112c056c70b67d4837e96bf2946db58
Leifhaa <Leifhaa@hotmail.com>
2020-05-29 15:32:48 +0200

    Fixes for login.

--- frontend/src/components/pages/login/loginForm.jsx
+++ frontend/src/components/pages/login/loginForm.jsx
@@ -12,6 +12,7 @@ export function LoginForm() {
             password: ""
         }
     ]);
+    const [errorMsg, setErrorMsg] = useState((''))
 
     function handleUserNameChange(e) {
         setLoginValue({userName: e.target.value});
@@ -22,11 +23,77 @@ export function LoginForm() {
         setLoginValue({password: e.target.value})
     } */
 
-    function handleSubmit(e) {
-        alert("Velkommen " + loginValue.userName + " til Restore, og takk for at du tenker på miljøet.");
-        e.preventDefault();
-        setLoginValue({userName: "", password: ""})
+
+    const handleError = (field, errmsg) => {
+        if(!field) return
+
+        if(errmsg) {
+            setErrorMsg('')
+        } else {
+            setErrorMsg(errorMsg)
+        }
     }
+
+    const renderError = () => {
+        if(errorMsg !== "") {
+            return <div className="error">{errorMsg}</div>
+        }
+    }
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+            const data = new FormData(event.currentTarget)
+            fetch('/perform_login', {
+                method: 'POST',
+                body: new URLSearchParams(event.currentTarget)
+            })
+                .then(v => {
+                    if(v.redirected) window.location = v.url
+                })
+                .catch(e => console.warn(e))
+    }
+
+    const doLogIn = async () => {
+
+        const url = "/api/perform_login";
+
+        const payload = { username: loginValue["userName"], password: loginValue["password"] };
+
+        let response;
+
+        let foo =  JSON.stringify(payload)
+
+        try {
+            response = await fetch(url, {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(payload)
+            });
+        } catch (err) {
+            this.setState({ errorMsg: "Failed to connect to server: " + err });
+            return;
+        }
+
+        if (response.status === 401) {
+            this.setState({ errorMsg: "Invalid userId/password" });
+            return;
+        }
+
+        if (response.status !== 204) {
+            this.setState({
+                errorMsg:
+                    "Error when connecting to server: status code " + response.status
+            });
+            return;
+        }
+
+        this.setState({ errorMsg: null });
+        //this.props.updateLoggedInUserId(userId);
+        this.props.history.push("/");
+    };
 
     return(
         <div className="login-container">
@@ -37,19 +104,17 @@ export function LoginForm() {
                     <div className="login-password">
                         Brukernavn (e-post)
                     </div>
-                    <input type="text" className="input-field" placeholder="Brukernavn" value={loginValue.userName}
+                    <input id="username" type="text" className="input-field" placeholder="Brukernavn" value={loginValue.userName}
                            onChange={handleUserNameChange}/>
                     <div className="login-password">
                         <div>Passord</div>
                         <Link to="">Glemt passord</Link>
                     </div>
-                    <input type="text" className="input-field" placeholder="Passord" value={loginValue.password}
+                    <input id="password" type="text" className="input-field" placeholder="Passord" value={loginValue.password}
                     />
                     {/*onChange={handlePasswordChange}*/}
                 </label>
-                <Link to="">
-                    <input type="submit" value="LOGG INN" className="submit p"/>
-                </Link>
+                <input type="submit" value="LOGG INN" className="submit p"/>
             </form>
         </div>
     )
