4d433d08fe39f3eb3ed6e49013da308e7b55edf7
Kjetil <kjetil.dir@gmail.com>
2020-06-02 13:44:12 +0200

    add test from other testproject, dont findt anything

--- frontend/src/tests/home/home-test.jsx
+++ frontend/src/tests/home/home-test.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import axiosMock from 'axios';
 import Home from '../../components/pages/home/home';
 
-jest.mock('axios');
 
 test('display restore', () => {
 
-    const {asFragment} = render(<Home />)
-    expect(asFragment(<Home />)).toMatchSnapshot();
+    const { getByText } = render(<Home />);
+    const linkElement = getByText(/RESTORE/i);
+    expect(linkElement).toBeInTheDocument();
+
+    //const {asFragment} = render(<Home />)
+   // expect(asFragment(<Home />)).toMatchSnapshot();
 });
 
