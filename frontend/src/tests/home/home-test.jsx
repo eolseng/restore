63c41847937dc880f162420f6a166d7f40e5b6a9
Kjetil <kjetil.dir@gmail.com>
2020-06-02 12:59:09 +0200

    add tests to src

--- frontend/src/tests/home/home-test.jsx
+++ frontend/src/tests/home/home-test.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import axiosMock from 'axios';
-import Home from '../../src/components/pages/home/home';
+import Home from '../../components/pages/home/home';
 
 jest.mock('axios');
 
 test('display restore', () => {
 
-    const {asFragment} = render(<Home text="Restore - pant dine klær i dag" />)
-    expect(asFragment()).toMatchSnapshot();
+    const {asFragment} = render(<Home />)
+    expect(asFragment(<Home />)).toMatchSnapshot();
 });
 
