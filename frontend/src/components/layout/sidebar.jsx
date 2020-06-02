56ef630b3eaf25160be38dbd7f1f285e80c8cae0
Kristian Garberg <kristian.garberg@gmail.com>
2020-06-02 10:34:55 +0200

    Added mobile phone support

--- frontend/src/components/layout/sidebar.jsx
+++ frontend/src/components/layout/sidebar.jsx
@@ -5,24 +5,24 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import logo from "../../img/logo/restore.png";
 
 
+
 function Sidebar(props) {
 
-    return (
-        <Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
-            <div id="logo">
-                <img id="logo-img" src={logo} alt="logo"/>
-            </div>
-            <a id="home" className="menu-item" href="/">Hjem</a>
-            <a id="how-to" className="menu-item" href="/">Slik gjør vi det</a>
-            <a id="tjenester" className="menu-item" href="/">Våre tjenester </a>
-
-            <a id="visjon" className="menu-item" href="/">Vår visjon</a>
-            <a id="profile" className="menu-item" href="/profile">
-                <FontAwesomeIcon className='profile-icon' icon={["far", "user-circle"]}/>
-                Min Side
-            </a>
-        </Menu>
-    );
+        return (
+            <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"}>
+                <div id="logo">
+                    <img id="logo-img" src={logo} alt="logo"/>
+                </div>
+                <a id="home" className="menu-item" href="/">Hjem</a>
+                <a id="how-to" className="menu-item">Slik gjør vi det</a>
+                <a id="tjenester" className="menu-item" href="/">Våre tjenester </a>
+                <a id="visjon" className="menu-item" href="/">Vår visjon</a>
+                <a id="profile" className="menu-item" href="/profile">
+                    <FontAwesomeIcon className='profile-icon' icon={["far", "user-circle"]}/>
+                    Min Side
+                </a>
+            </Menu>
+        );
 }
 
 export default Sidebar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 
-
 import Header from "./components/layout/header";
 import Footer from "./components/layout/footer";
 import Home from "./components/pages/home/home";
-
+import Sidebar from "./components/layout/sidebar";
+//https://github.com/haldarmahesh/use-mobile-detect-hook
+import useMobileDetect from "use-mobile-detect-hook";
 import {Profile} from "./components/pages/profile/profile";
 import Restore from "./components/pages/restore/restore";
 import {RestoreContextProvider} from "./components/pages/restore/restoreContext";
+
 
 /** CSS Imports */
 import './css/style.css'
@@ -19,7 +21,7 @@ import {library} from '@fortawesome/fontawesome-svg-core'
 import {faBars, faTimes, faLaptop, faSave, faEdit, faChevronDown, faSignInAlt} from '@fortawesome/free-solid-svg-icons'
 import {faUser} from '@fortawesome/free-regular-svg-icons'
 import {faFacebookSquare, faTwitterSquare, faLinkedin, faInstagramSquare} from '@fortawesome/free-brands-svg-icons'
-import Sidebar from "./components/layout/sidebar";
+
 
 /** Font Awesome Icon Library */
 library.add(
@@ -50,36 +52,62 @@ function App() {
     const path = window.location.pathname;
     const outerContainerId = "outer-container";
     const pageWrapId = "page-wrap";
+    const detectMobile = useMobileDetect();
 
-    return (
-        <React.StrictMode>
-            <BrowserRouter>
-                <div id={outerContainerId} style={{height: '100%'}}>
-                    <Sidebar pageWrapId={"page-wrap"} outerContainerId={"outer-container"}/>
-                    <main id={pageWrapId}>
-                        <div id='main-container'>
-                            {path !== '/' && Header}
-                            <Switch>
-                                {/* Husk å legge inn routen i no.repairable.backend.controller.ReactForwardController */}
-                                <Route component={Home} exact path={'/'}/>
-                                <Route exact path="/profile" render={(props) => <Profile {...props}/>}/>
-                                <Route exact path={"/restore"}>
-                                    <RestoreContextProvider>
-                                        <Restore/>
-                                    </RestoreContextProvider>
-                                </Route>
-                                <Route path='/404' component={notFound}/>
-                                <Redirect to='/404'/>
-                            </Switch>
-                            <Footer/>
-                        </div>
-                    </main>
-                </div>
-            </BrowserRouter>
-        </React.StrictMode>
-    );
+    if (detectMobile.isMobile()) {
+        return (
+            <React.StrictMode>
+                <BrowserRouter>
+                    <div id={outerContainerId} style={{height: '100%'}}>
+                        <Sidebar pageWrapId={"page-wrap"} outerContainerId={"outer-container"}/>
+                        <main id={pageWrapId}>
+                            <div id='main-container'>
+                                {path !== '/'}
+                                <Switch>
+                                    {/* Husk å legge inn routen i no.repairable.backend.controller.ReactForwardController */}
+                                    <Route component={Home} exact path={'/'}/>
+                                    <Route exact path="/profile" render={(props) => <Profile {...props}/>}/>
+                                    <Route exact path={"/restore"}>
+                                        <RestoreContextProvider>
+                                            <Restore/>
+                                        </RestoreContextProvider>
+                                    </Route>
+                                    <Route path='/404' component={notFound}/>
+                                    <Redirect to='/404'/>
+                                </Switch>
+                                <Footer/>
+                            </div>
+                        </main>
+                    </div>
+                </BrowserRouter>
+            </React.StrictMode>
+        );
+
+    } else {
+
+        return (
+            <React.StrictMode>
+                <BrowserRouter>
+                    <div id='main-container'>
+                        {path !== '/' && Header}
+                        <Switch>
+                            {/* Husk å legge inn routen i no.repairable.backend.controller.ReactForwardController */}
+                            <Route component={Home} exact path={'/'}/>
+                            <Route exact path="/profile" render={(props) => <Profile {...props}/>}/>
+                            <Route exact path={"/restore"}>
+                                <RestoreContextProvider>
+                                    <Restore/>
+                                </RestoreContextProvider>
+                            </Route>
+                            <Route path='/404' component={notFound}/>
+                            <Redirect to='/404'/>
+                        </Switch>
+                        <Footer/>
+                    </div>
+                </BrowserRouter>
+            </React.StrictMode>
+        );
+    }
 }
 
-ReactDOM.render(
-    <App/>
-    , document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'));
