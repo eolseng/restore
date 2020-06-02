beeb9a6dc58e8432b66e9066ba0d3d997f99167b
Kjetil <kjetil.dir@gmail.com>
2020-06-02 15:16:47 +0200

    add 2 tests

--- frontend/src/__tests__/home/home.test.js
+++ frontend/src/__tests__/home/home.test.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Home from '../../components/pages/home/home';
+import {BrowserRouter} from "react-router-dom";
 
+afterEach(cleanup)
+
+test('should take snapshot', () => {
+    const {asFragment} = render(
+        <BrowserRouter>
+            <Home/>
+        </BrowserRouter>);
+
+    expect(asFragment(<Home/>)).toMatchSnapshot()
+});
 
 test('display restore', () => {
-/*
-    const { getByText } = render(<Home />);
-    const linkElement = getByText(/RESTORE/i);
-    expect(linkElement).toBeInTheDocument(); */
 
-    //const {asFragment} = render(<Home />)
-   // expect(asFragment(<Home />)).toMatchSnapshot();
+    const { getByText } = render(<BrowserRouter><Home /></BrowserRouter>);
+    const h1 = getByText(/Restore - pant dine klær i dag/);
+    expect(h1).toBeInTheDocument();
 });
+
 
