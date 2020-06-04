3754ccee79c4246a020953da8dd8c3bef807789f
Leif Erik <42612944+Leifhaa@users.noreply.github.com>
2020-06-04 17:54:13 +0200

    Feature/IntegrationTests (#73)
    * Added integration tests for selecting a product.
    
    * Clicked size.

--- frontend/src/components/pages/home/home.jsx
+++ frontend/src/components/pages/home/home.jsx
@@ -122,7 +122,7 @@ function Home() {
                                 klær!</b>
                             <div className='button-container'>
                                 <Link className='button-link' data-testid="start-button-link" to='/restore'>
-                                    <button className='cta-button' data-testid='start-button'>Start nå</button>
+                                    <button id="startBtn" className='cta-button' data-testid='start-button'>Start nå</button>
                                 </Link>
                             </div>
                         </div>
@@ -141,7 +141,7 @@ function Home() {
                         <div className='header-left'>
                             <span
                                 className={activeNavMenuIdx === 0 ? 'active-nav-button' : null}
-                                onClick={renderBlueprint} data-testid="how-it-works"
+                                onClick={renderBlueprint} data-testid="how-it-works" id="btnHowItWorks"
                             >
                                 Slik fungerer det
                             </span>
