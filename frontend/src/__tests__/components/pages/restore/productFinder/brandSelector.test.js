a10293260ed7df466d7e88519b009695789ebb0e
Kjetil <kjetil.dir@gmail.com>
2020-06-04 21:17:36 +0200

    fix and made test for brandSelection, and moved and renamed product-filter.test -> productCard-test

--- frontend/src/__tests__/components/pages/restore/productFinder/brandSelector.test.js
+++ frontend/src/__tests__/components/pages/restore/productFinder/brandSelector.test.js
@@ -1,53 +1,37 @@
-import React, {useContext} from "react";
+import React, {useContext, useReducer} from "react";
 import { render, cleanup, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import BrandSelector from "../../../../../components/pages/restore/brandSelector";
+import {RestoreContext} from "../../../../../components/pages/restore/restoreContext";
 
 afterEach(cleanup)
 
-test('display button: Det Brand ID', () => {
-    let brandId = 0;
-    function mockSetBrandId () {
-        brandId += 1;
-    }
-
-    const { getByText } = render(
-        <button onClick={mockSetBrandId(1)}>Set Brand ID!</button>);
-
-    const button = getByText('Set Brand ID!');
-    expect(button).toBeInTheDocument();
-});
-
-test('display button: next step', () => {
-    const mockState = {
-        maxSteps: 4,
-        step: 1,
-        brandId: null
-    };
-    function prevStep() {
-        mockState.step -= 1;
-    }
-    function nextStep() {
-        mockState.step += 1;
-    }
-    function setBrandId() {
-    }
-    const { getByText, getByTestId } = render(
-        <div>
-            <h1>BrandSelector</h1>
-            <p data-testid='Max Steps'>Max Steps: {mockState.maxSteps}</p>
-            <p data-testid='Step'>Step: {mockState.step}</p>
-            <button onClick={prevStep}>Previous Step!</button>
-            <button onClick={nextStep}>Next Step!</button>
-            <button onClick={setBrandId(1)}>Set Brand ID!</button>
-        </div>
+// A function that can be used in many tests
+function renderBrandSelector(value) {
+   return render(
+        <RestoreContext.Provider value={value}>
+            <BrandSelector/>
+        </RestoreContext.Provider>
     );
+}
+
+// Mock state that i the same as the original with the values needed and can be used in many tests
+const state = {
+    maxSteps: 1,
+    step: 1,
+    brandId: null,
+};
+
+const value = {state};
+
+test('does it render', () => {
+
+    const { getByText, getByTestId } = renderBrandSelector(value);
+
     expect(getByText('BrandSelector')).toBeInTheDocument()
     expect(getByTestId('Max Steps')).toBeInTheDocument()
-    expect(getByTestId('Step')).toBeInTheDocument()
+    expect(getByTestId('step')).toBeInTheDocument();
     expect(getByText('Next Step!')).toBeInTheDocument()
     expect(getByText('Previous Step!')).toBeInTheDocument()
-    fireEvent.click(getByText('Next Step!'));
-    console.log(mockState.step)
-    expect(getByTestId('Step')).toHaveTextContent('Step: 2')
+    expect(getByText('Set Brand ID!')).toBeInTheDocument()
 });
--- frontend/src/__tests__/components/pages/restore/productFinder/productCard.test.js
+++ frontend/src/__tests__/components/pages/restore/productFinder/productCard.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {render} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import {ProductCard} from "../../components/pages/restore/productFinder/productCard";
+import {ProductCard} from "../../../../../components/pages/restore/productFinder/productCard";
 
 test('Render product card', () => {
         const name = "test_name"
