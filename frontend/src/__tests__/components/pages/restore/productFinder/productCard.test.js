edf28569845751f2ea38e381ffa3cf285c92ec75
Kjetil <kjetil.dir@gmail.com>
2020-06-05 10:55:43 +0200

    tester ut noe med productCard tester

--- frontend/src/__tests__/components/pages/restore/productFinder/productCard.test.js
+++ frontend/src/__tests__/components/pages/restore/productFinder/productCard.test.js
@@ -2,7 +2,33 @@ import React from "react";
 import {render} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import {ProductCard} from "../../../../../components/pages/restore/productFinder/productCard";
+import {RestoreContext} from "../../../../../components/pages/restore/restoreContext";
+import RestoreProgress from "../../../../../components/pages/restore/restoreProgress";
+/*
+// A function that can be used in many tests
+function renderProductCard(value) {
+    return render(
+        <RestoreContext.Provider value={value}>
+            <ProductCard product={mockProduct} imageIndex={0}/>
+        </RestoreContext.Provider>
+    );
+}
 
+const name = "test_name"
+const mockProduct = {
+    id: 123,
+    _links: {
+        self: {
+            href: "something.com"
+        }
+    },
+    name: name,
+    images: [{
+        imgUrl: "somethingg.com",
+        colorName: "hva enn jeg vil"
+    }],
+}
+*/
 test('Render product card', () => {
         const name = "test_name"
         const mockProduct = {
