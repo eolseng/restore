084df15091def61c3c32d87414880a1ad6ba0cde
Haakon Hess <haakon.hess@gmail.com>
2020-06-05 11:06:44 +0200

    General layout and styling

--- frontend/src/components/pages/restore/thankYou.jsx
+++ frontend/src/components/pages/restore/thankYou.jsx
@@ -1,25 +1,32 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 import '../../../css/pages/restore/thankYou/thankYou.css'
 
 function ThankYou() {
-
     return (
         <div className={'container-fluid'}>
-            <div className={'container'}>
-                <h1>Tusen takk!</h1>
-                <p>Tusen takk for at du har benyttet deg av Restore by Repairable</p>
-                <p>Du vil innen kort tid motta en epost med informasjon.</p>
-                <p>Skulle du ha noen spørsmål tilknyttet din ordre? Send oss en epost på hei@repairable.no!</p>
-                <p>Dette er slutten</p>
-                <Link to={"/"}>
-                    Gå tilbake til hovedsiden
-                </Link>
+            <div className={'container thankyou-container'}>
+                <div className='thankyou-content'>
+                    <div className='image-content'>
+                        <img className="thankyou-image" src={require('../../../img/thankYou/super-thank-you.svg')} alt='Tusen takk' />
+                    </div>
+                    <div className='text-content'>
+                        <h1>Tusen takk!</h1>
+                        <p>Tusen takk for at du har benyttet deg av Restore by Repairable.</p>
+                        <p>Du vil innen kort tid motta en epost med mer informasjon.</p>
+                        <p>Skulle du ha noen spørsmål tilknyttet din ordre? Send oss en epost på hei@repairable.no!</p>
+                        <div className='continue-button-container'>
+                    <Link to={'/'}>
+                        <button className='continue-button cta-button'>Gå tilbake til hovedsiden</button>
+                    </Link>
+                </div>
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
-
 }
 
-export default ThankYou;
+export default ThankYou
