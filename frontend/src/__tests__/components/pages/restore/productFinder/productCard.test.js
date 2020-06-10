49f434bc46f06820e5f40e20816b45781972cb5f
Kjetil Haug Terjesen <42612983+Dofsevenn@users.noreply.github.com>
2020-06-10 14:25:17 +0200

    Feature/test3 kjetil (#80)
    * tester ut noe med productCard tester
    
    * just to pull dev
    
    * last test

--- frontend/src/__tests__/components/pages/restore/productFinder/productCard.test.js
+++ frontend/src/__tests__/components/pages/restore/productFinder/productCard.test.js
@@ -1,26 +1,47 @@
 import React from "react";
-import {render} from '@testing-library/react';
+import {render, cleanup, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import {ProductCard} from "../../../../../components/pages/restore/productFinder/productCard";
+import {RestoreContext} from "../../../../../components/pages/restore/restoreContext";
 
-test('Render product card', () => {
-        const name = "test_name"
-        const mockProduct = {
-            id: 123,
-            _links: {
-                self: {
-                    href: "something.com"
-                }
-            },
-            name: name,
-            images: [{
-                imgUrl: "somethingg.com",
-                colorName: "hva enn jeg vil"
-            }],
+afterEach(cleanup);
+
+// A function that can be used in many tests
+function renderProductCard(value) {
+    return render(
+        <RestoreContext.Provider value={value}>
+            <ProductCard product={mockProduct} imageIndex={0}/>
+        </RestoreContext.Provider>
+    );
+}
+
+//Mocking data for product
+const name = "test_name"
+const mockProduct = {
+    id: 123,
+    _links: {
+        self: {
+            href: "something.com"
         }
+    },
+    name: name,
+    images: [{
+        imgUrl: "somethingg.com",
+        colorName: "hva enn jeg vil"
+    }],
+};
 
-        const {getByText} = render(<ProductCard product={mockProduct} imageIndex={0}/>)
-        expect(getByText(name)).toBeInTheDocument()
-        // expect(getByText(mockProduct.id)).toBeInTheDocument()
-    }
-)
+const state = {
+    maxSteps: 1,
+    step: 1,
+    brandId: null,
+};
+
+const value = [ {state}, {mockProduct} ];
+
+test('Render product card, and check that display card has attribute id', () => {
+    const { getByText, getByTestId} = renderProductCard(value);
+
+    expect(getByText(name)).toBeInTheDocument();
+    expect(getByTestId('displayCard')).toHaveAttribute('id')
+});
